070f5f9093a234d6e94b3e27924649fcf76ecf13
staple91 <a998666@naver.com>
2023-09-27 21:32:09 +0900

    124O
    2132456

--- Rhythm/Assets/Script/JudgeComponent.cs
+++ Rhythm/Assets/Script/JudgeComponent.cs
@@ -14,6 +14,7 @@ public class JudgeComponent : MonoBehaviour, IJudgeable
 {
     public Result Judge(float dist)
     {
+        //JudgeRange를 클래스로 해서 next범위를 가지고있고 foreach문을 돌려버린다.
         // 공통 - 노트 dequeue해서 삭제(objpoolReturn) / 마지막빼고.
         if (dist < JudgeRange.perfectRange)
         {
